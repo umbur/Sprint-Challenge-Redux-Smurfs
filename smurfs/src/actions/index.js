e63d4f881f3a1ad38d55d103b83f2f4003c627d2
umbur <work1415@gmail.com>
2019-02-01 10:30:53 -0700

    POST request works

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,7 +4,12 @@ export const FETCHING_SMURFS = 'FETCHING_SMURFS';
 export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
 export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
 
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDING_SMURF_SUCCESS = 'ADDING_SMURF_SUCCESS';
+export const ADDING_SMURF_FAILURE = 'ADDING_SMURF_FAILURE';
+
 const url = 'http://localhost:3333/smurfs'
+
 export const getSmurfs = () => dispatch => {
     dispatch({type: FETCHING_SMURFS});
     axios.get(url)
@@ -17,3 +22,16 @@ export const getSmurfs = () => dispatch => {
             payload: data
         }))
 };
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({type: ADDING_SMURF});
+  axios.post(url, smurf)
+      .then(({data}) => dispatch({
+          type: ADDING_SMURF_SUCCESS,
+          payload: data
+      }))
+      .catch(({data}) => dispatch({
+          type: ADDING_SMURF_FAILURE,
+          payload: data
+      }));
+}
